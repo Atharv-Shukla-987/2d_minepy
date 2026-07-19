1af65756268881364493b5a852d4d2a867947acf
Atharv-Shukla-987 <249597135+Atharv-Shukla-987@users.noreply.github.com>
2026-07-19 18:32:46 +0530

    texs

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import random
 app = Ursina()
 
 blocks = {}
+created_chucks = set()
+removed = []
 Player = Entity(
     model="quad",
     texture='tex/up.png', 
@@ -141,37 +143,74 @@ p = [70,
      0.01]
 
 facing = (0,1)
-def snap_to_grid(v):
-   return round(round(v/(1/3))*(1/3),4)
-
+foot_offset= (Player.scale_y - 1)/2
 def tar_blk_pos():
-   return snap_to_grid(Player.x + facing[0]*(1/3)),snap_to_grid(Player.y + facing[1]*(1/3))
+   return round(Player.x + facing[0]),round(Player.y + facing[1])
+
 def chuck(x,y):
+    if (x,y) in created_chucks:
+       return
+    created_chucks.add((x,y))
     for _x in range(6):
         __x = round(x + _x)
         for _y in range(6):
             __y = round(y + _y)
             if (__x,__y) in spawn_point:
                 continue
+            if (__x,__y) in removed:
+               continue
+            
             if (__x,__y) not in blocks:
                blocks[(__x,__y)] = random.choices(blks,weights=p,k=1)[0](__x,__y)
+               
                 
 
 
 spawn_point =[(0,0),(1,0),(0,1),(1,1),(-1,0),(0,-1),(-1,-1),(1,-1),(-1,1)]
 
-def can_move(dir,dis):
-   hit_info = raycast(Player.world_position,dir,distance=dis,ignore=(Player,) ,debug=False)
-   return not hit_info.hit
-
+def can_move(dir,move):
+   buffer = 0.1
+   half_h = Player.scale_y / 2 - 0.05
+   half_w = Player.scale_x /2 - 0.05
+   if dir.x != 0:
+      dist = Player.scale_x / 2 + move + buffer
+      offsets = [Vec3(0,-half_h,0),Vec3(0,half_h,0)]
+   else:
+      dist = Player.scale_x / 2 + move + buffer
+      offsets = [Vec3(-half_w,0,0),Vec3(half_w,0,0)]
+   
+   for off in offsets:
+      origin = Player.world_position + off
+      hit_info = raycast(origin,dir,distance=dist,ignore=(Player,),debug=False)
+      if hit_info.hit :
+         return False
+      return True
+   
+def unload():
+   pxch = (int(Player.x)//6)*6
+   pych = (int(Player.y)//6)*6
+   for chpos in list(created_chucks):
+      cx,cy = chpos
+      dist = ((cx-pxch)**2 + (cy - pych)**2)**0.5
+      if dist >  24 :
+         for x_ in range(6):
+            for y_ in range(6):
+               key = (cx+x_,cy+y_)
+               if key in blocks:
+                  destroy(blocks[key])
+                  del blocks[key]
+         
+         created_chucks.discard(chpos)
 def update():
    px = int(Player.x)
    py = int(Player.y)
-   for x in range(px-7,px +8):
-      for y in range(py-5,py+6):
-         if (x,y) not in spawn_point :
-          if x%2 and y%2 :
-                chuck(x,y)
+   pych = (py//6)*6
+   pxch = (px//6)*6
+   for x in range(pxch-12,pxch+18,6):
+      for y in range(pych-12,pych+18,6):
+         chuck(x,y)
+   unload()
+
    
 
 def input(key):
@@ -179,31 +218,39 @@ def input(key):
     move = 5*time.dt
     check = move + 0.25
     if held_keys["w"]:
-        if can_move(Vec3(0,1,0),check):
+        if can_move(Vec3(0,1,0),move):
            Player.y += move
         Player.texture = 'tex/up.png'
         facing = (0,1)
     if held_keys["s"]:
-        if can_move(Vec3(0,-1,0),check):
+        if can_move(Vec3(0,-1,0),move):
            Player.y -= move
         Player.texture = 'tex/down.png'
         facing = (0,-1)
     if held_keys["d"]:
-        if can_move(Vec3(1,0,0),check):
+        if can_move(Vec3(1,0,0),move):
            Player.x += move
         Player.texture = 'tex/right.png'
         facing = (1,0)
     if held_keys["a"]:
-        if can_move(Vec3(-1,0,0), check):
+        if can_move(Vec3(-1,0,0), move):
            Player.x -= move
         Player.texture = 'tex/left.png'
         facing = (-1,0)
     if key == "e":
-       tx , ty = tar_blk_pos()
+       tx , ty = round(Player.x) + facing[0] , round(1+Player.y - foot_offset)
        if (tx,ty) in blocks:
           destroy(blocks[(tx,ty)])
           del blocks[(tx,ty)]
-     
+          removed.append((tx,ty))
+    
+    if key == "r":
+       tx , ty = round(Player.x) + facing[0] , round(Player.y - foot_offset)
+       if (tx,ty) in blocks:
+          destroy(blocks[(tx,ty)])
+          del blocks[(tx,ty)]
+          removed.append((tx,ty))
+
     if key == "f":
        tx ,ty = tar_blk_pos()
        if (tx,ty) not in blocks:
